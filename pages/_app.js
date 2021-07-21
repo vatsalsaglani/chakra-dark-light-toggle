64497334ba909e7c382bf61be56ae6f8e3689ebb
vatsals <vatsals@quinnox.com>
2021-07-21 18:01:35 +0530

    added chakra ui packages and wrapped app component inside chakraprovider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider, useColorMode } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { colorMode, toggleColorMode } = useColorMode();
+  console.log("Default color mode: ", colorMode);
+
+  return (
+    <ChakraProvider resetCSS>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
